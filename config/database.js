9f5c4df37b18178d1a1afcde3f6cab3aa2f5bc4c
hjn66 <h.niloofar@gmail.com>
2018-09-06 15:42:33 +0430

    test2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
      database: 'mongodb://julia:Julia123@localhost:27017/juliadb',
-     secret: 'KnbZMz7QUfjoyd2j1536148164662'
+     secret: 'KnbZMz7QUfjoyd2j15361481646621'
 }
