9525b5343c059a9757952eb6076f7edc1d586d4c
hjn66 <h.niloofar@gmail.com>
2018-09-06 15:51:23 +0430

    .gitignore fix

--- config/admin.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-     email: 'admin@admin.com',
-     pass: '123456',
-     firstName: 'admin',
-     lastName:'admin',
-}
--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-     database: 'mongodb://julia:Julia123@localhost:27017/juliadb',
-     secret: 'KnbZMz7QUfjoyd2j15361481646621'
-}
